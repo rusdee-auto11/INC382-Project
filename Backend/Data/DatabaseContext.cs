ac441586d7c3f51f1d136fb2ece5513f6c94b3ae
rusdee-auto11 <rusdeeza@gmail.com>
2020-05-11 04:55:26 +0700

    first commit

---
+++ Backend/Data/DatabaseContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Backend.Models;
+
+namespace Backend.Data
+{
+    public partial class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        {
+        }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TagValue> TagValue { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TagValue>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Tagname)
+                    .IsRequired()
+                    .HasColumnName("tagname")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Value).HasColumnName("value");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Backend/Models/TagValue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Backend.Models
+{
+    public partial class TagValue
+    {
+        public int Id { get; set; }
+        public string Tagname { get; set; }
+        public double Value { get; set; }
+    }
+}
